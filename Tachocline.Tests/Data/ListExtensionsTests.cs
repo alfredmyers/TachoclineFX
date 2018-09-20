8d84c00aeac1c6cbfb829733225d78a2a8236aba
Alfred Myers <git@alfredmyers.com>
2018-09-19 22:21:32 -0300

    Trimmed ListExtensions & added unit tests for it

---
+++ Tachocline.Tests/Data/ListExtensionsTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+using Tachocline.Data;
+using System.Linq;
+
+public class ListExtensionsTests
+{
+    private readonly IList _list;
+    private readonly IEnumerable<int> _range;
+
+    public ListExtensionsTests()
+    {
+        _list = new ArrayList { null, 42, "Name", DBNull.Value };
+        _range = Enumerable.Range(0, _list.Count);
+    }
+
+    [Fact]
+    public static void NullListThrowsArgumentNullException()
+    {
+        IList list = null;
+        Assert.Throws<ArgumentNullException>(() =>
+            list.GetValue<int>(0)
+        );
+    }
+
+    [Fact]
+    public void GetValueType()
+    {
+        var index = 1;
+        Assert.Equal(_list[index], _list.GetValue<int>(index));
+    }
+
+    [Fact]
+    public void GetReferenceType()
+    {
+        var index = 2;
+        Assert.Equal(_list[index], _list.GetValue<string>(index));
+    }
+
+    [Fact]
+    public void GetNullReferenceType()
+    {
+        var index = 0;
+        Assert.Equal(_list[index], _list.GetValue<string>(index));
+    }
+
+    [Fact]
+    public void GetObject() => Assert.Equal(_list, _range.Select(_list.GetValue).ToList());
+
+    [Fact]
+    public void IsDBNull() => Assert.Equal(new bool[] { false, false, false, true }, _range.Select(_list.IsDBNull));
+
+    [Fact]
+    public void GetFieldType() => Assert.Equal(new Type[] { typeof(int), typeof(string), typeof(DBNull) }, _range.Skip(1).Select(_list.GetFieldType));
+}
--- Tachocline/Data/ListExtensions.cs
+++ Tachocline/Data/ListExtensions.cs
@@ -1,84 +1,32 @@
 ﻿using System;
 using System.Collections;
-using System.Data;
 
 namespace Tachocline.Data
 {
     public static class ListExtensions
     {
-        public static bool GetBoolean(this IList values, int i)
-        {
-            return (bool)values[i];
-        }
-
-        public static byte GetByte(this IList values, int i)
-        {
-            return (byte)values[i];
-        }
-
-        public static char GetChar(this IList values, int i)
-        {
-            return (char)values[i];
-        }
-
-        public static DateTime GetDateTime(this IList values, int i)
-        {
-            return (DateTime)values[i];
-        }
-
-        public static decimal GetDecimal(this IList values, int i)
-        {
-            return (decimal)values[i];
-        }
-
-        public static double GetDouble(this IList values, int i)
-        {
-            return (double)values[i];
-        }
-
-        public static Type GetFieldType(this IList values, int i)
-        {
-            return values[i].GetType();
-        }
-
-        public static float GetFloat(this IList values, int i)
-        {
-            return (float)values[i];
-        }
-
-        public static Guid GetGuid(this IList values, int i)
-        {
-            return (Guid)values[i];
-        }
-
-        public static short GetInt16(this IList values, int i)
-        {
-            return (short)values[i];
-        }
-
-        public static int GetInt32(this IList values, int i)
-        {
-            return (int)values[i];
-        }
-
-        public static long GetInt64(this IList values, int i)
-        {
-            return (long)values[i];
-        }
-
-        public static string GetString(this IList values, int i)
-        {
-            return (string)values[i];
-        }
+        public static Type GetFieldType(this IList values, int i) => values.GetValue(i).GetType();
 
         public static object GetValue(this IList values, int i)
         {
+            if (values is null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
             return values[i];
         }
 
-        public static bool IsDBNull(this IList values, int i)
+        public static T GetValue<T>(this IList values, int i)
         {
-            return values[i] == DBNull.Value;
+            if (values is null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            return (T)values[i];
         }
+
+        public static bool IsDBNull(this IList values, int i) => values[i] == DBNull.Value;
     }
 }
